218bdbf4183a78c7ba36d1a994a0fdf1d7988685
Ahmed-Elkomy <ahmed.elkomy87@gmail.com>
2020-02-15 13:04:44 +0200

    Update the Device Screen

--- lib/screens/device_screen.dart
+++ lib/screens/device_screen.dart
@@ -1,10 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 
 class DeviceScreen extends StatefulWidget {
   final title;
   final BluetoothDevice device;
+
   DeviceScreen({this.title, this.device});
+
   @override
   _DeviceScreenState createState() => _DeviceScreenState();
 }
@@ -19,9 +23,43 @@ class _DeviceScreenState extends State<DeviceScreen> {
       body: SafeArea(
         child: Container(
           padding: EdgeInsets.all(10),
-          child: Center(child: Text("To be finished later")),
+          child: StreamBuilder<List<BluetoothService>>(
+            stream: widget.device.services,
+            initialData: [],
+            builder: (c, snapshot) {
+              _characteristicScanning(snapshot);
+              return ListView(
+                children: snapshot.data
+                    .map(
+                      (bluetoothService) => Card(
+                        child: Text(bluetoothService.uuid.toString()),
+                      ),
+                    )
+                    .toList(),
+              );
+            },
+          ),
         ),
       ),
     );
+  }
+
+  void _characteristicScanning(AsyncSnapshot<List<BluetoothService>> snapshot) {
+    Stream<List<int>> stream;
+    List<BluetoothService> services = snapshot.data;
+    services.forEach((service) {
+      service.characteristics.forEach((character) {
+        print('character uuid = ${character.uuid}');
+        Stream<List<int>> stream = character.value;
+        stream.listen((snapshot) {
+          String characterValue = _dataParser(snapshot);
+          print('character value = ${characterValue}');
+        });
+      });
+    });
+  }
+
+  String _dataParser(List<int> dataFromDevice) {
+    return utf8.decode(dataFromDevice);
   }
 }
